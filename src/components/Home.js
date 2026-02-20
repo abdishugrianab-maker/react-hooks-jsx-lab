325e0264e65d7633d2bb783ea971648d84e986d4
anab ali <abdishugrianab@gmail.com>
2026-02-20 10:12:52 +0300

    done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
 import React from "react";
-import { name, city } from "../data/data.js";
 
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+    <div id="home">
+      <h1 style={{ color: "firebrick" }}>
+        Liza is a Web Developer from New York
+      </h1>
+    </div>
+  );
 }
 
 export default Home;
